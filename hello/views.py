0d079757addc9e51d150d0d53042b142f926ddb9
yusufRahmatullah <ycingle@gmail.com>
2017-02-13 22:38:02 +0700

    add commands handle add help text

--- hello/views.py
+++ hello/views.py
@@ -1,15 +1,18 @@
-import re
-
 import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import *
-from .worker import parse
+from .worker import exec_command
 
 CHANNEL_ACCESS_TOKEN = "xjNlgBf2p8rHke5W5iu/gNXjci1p0navxxgqyK3CRPVdHRvFoWhd9QT7YK/YqAlhU7eXAK3Sb91X3DAGv67jn9vGIgjHMBnGtZ7U019Xa2gQeHMvd1vg6LCscRw5EqS+5KcC7+RZV9QcRX8nfLkBLwdB04t89/1O/w1cDnyilFU="
 LINE_API_REPLY = "https://api.line.me/v2/bot/message/reply"
+LINE_API_PUSH = "https://api.line.me/v2/bot/message/push"
+LINE_API_HEADERS = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer {}".format(CHANNEL_ACCESS_TOKEN)
+}
 
 
 # Create your views here.
@@ -33,21 +36,17 @@ def linebot(request):
         events_container = EventsContainer(request.body)
         event = events_container.events[0]
         if event.message.type == 'text':
-            result = _exec_command(event.message.text.lower())
+            result = exec_command(event.message.text.lower())
         else:
             result = 'We only receive text message'
         reply_message = ReplyMessage(event.reply_token, [Message.from_string(result)])
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer {}".format(CHANNEL_ACCESS_TOKEN)
-        }
-        res = requests.post(LINE_API_REPLY, data=reply_message.to_json(), headers=headers)
+        res = requests.post(LINE_API_REPLY, data=reply_message.to_json(), headers=LINE_API_HEADERS)
         print res.text
         return HttpResponse(reply_message.to_json())
 
 
-def _exec_command(message):
-    if 'help' in message:
-        return 'Help page is under construction'
-    else:
-        return parse(message)
+def _send_push(target_id, message):
+    body = dict()
+    body['to'] = target_id
+    body['messages'] = [Message(message)]
+    res = requests.post(LINE_API_PUSH, data=json.dumps(body), headers=LINE_API_HEADERS)
--- hello/worker.py
+++ hello/worker.py
@@ -4,6 +4,216 @@ import re
 
 import sympy
 
+ABOUT_TEXT = """ MathCalc - Utility to help you solve mathematics problems
+This bot is created by anakarwin@gmail.com.
+Send me suggestion to support this bot or just line me id: anakarwin.
+This bot is created using python programming language,
+using library called sympy (http://www.sympy.org/en/index.html).
+All available commands are store in their website, try it your self.
+Believe me, everyone can code. Learn how to code in http://code.org.
+You can create your own program. If you wanna share or ask anything about programming, please tell me.
+
+--------
+MathCalc version 0.1.0
+"""
+COMMANDS_HELP = {
+    "diff": """ diff(expr) - return derivative of expr
+ex:
+diff(cos(x))
+>>  result:
+    -sin(x)
+""",
+    "factor": """ factor(expr) - return factor of expr
+ex:
+factor(x**2 - 2*x - 8)
+>>> result:
+    (x - 4)*(x + 2)
+""",
+    "integrate": """ integrate(expr) - return integral of expr
+ex:
+integrate(sin(x))
+>>> result:
+    -cos(x)
+""",
+    "series": """ series(expr) - return taylor series of expr
+ex:
+series(sin(x))
+>>> result:
+    x - x^3/6 + x^5/120 + O(x^6)
+""",
+    "expand_tri": """ expand_trig(expr) - expand trigonometry of expr
+ex:
+expand_tri(sin(2**x))
+>>> result:
+    2*sin(x)*cos(x)
+
+expand_tri(sin(x + y))
+>>> result:
+    sin(x)*cos(y) + sin(y)*cos(x)
+""",
+    "sqrt": """ sqrt(expr) - square root of expr
+ex:
+sqrt(1/100)
+>>> result:
+    1/10
+""",
+    "log": """ log(expr) - natural logarithm of expr (base 'e')
+log(expr, b) - logarithm of expr with base 'b'
+log(expr, b) execute as log(expr) / log(b)
+ex:
+log(exp(1))
+>>> result:
+    1
+
+log(4, 2)
+>>> result:
+    2
+
+log(1/4, 2)
+>>> result:
+    -1.38629436111989/log(2)
+
+[CHEAT] using .evalf() in the end to get the result
+ex:
+log(1/4, 2).evalf()
+>>> result:
+    -2.00000000000
+""",
+    "solve": """ solve(expr) - solve equations of expr
+returns array of solution
+ex:
+solve(x^2 + 2*x + 1)
+>>> result:
+    [-1]
+""",
+    "abs": """ abs(expr) - absolute of expr
+ex:
+abs(sin(-pi/2))
+>>> result:
+    1
+""",
+    "expand": """ expand(expr) - expand polynomial from expr
+ex:
+expand((x + 1)^2)
+>>> result:
+    x^2 + 2*x + 1
+""",
+    "limit": """ limit(expr, base, target) - limit of base to target from expr
+using double o (oo) as infinity
+ex:
+limit(1/x, x, 0)
+>>> result:
+    oo
+
+[CHEAT] using forth parameter '+' or '-' to evaluate limit one sided
+ex:
+limit(1/x, x, 0, '-')
+>>> result:
+    -oo
+""",
+    "matrix": """ Matrix can be used by passing matrices
+ex:
+create matrix with 2 row 1 column
+| 1 |
+| 2 |
+
+Matrix([1, 2])
+>>> result:
+Matrix([1, 2])
+
+--------------
+create matrix with 1 row 2 column
+| 1 2 |
+
+Matrix([[1, 2]])
+>>> result:
+Matrix([[1, 2]])
+
+--------------
+multiply matrix 2x1 with 1x2
+| 1 | * | 1 2 |
+| 2 |
+
+| 1 2 |
+| 2 4 |
+
+Matrix([1,2]) * Matrix([[1,2]])
+>>> result:
+    Matrix([[1,2], [2,4]])
+
+--------------
+determinant - add .det() as suffix
+Matrix([[1,2], [2,3]])
+>>> result:
+    -1
+
+--------------
+transpose - add .T as suffix
+Matrix([1,2], [3,4])
+>>> result:
+    Matrix([[1, 3], [2, 4]])
+"""
+}
+DEFAULT_HELP = """ MathCalc - Utility to help you solve mathematics problems
+Type 'help' to show this message.
+You can compute simple math as '1+1' or more complex math using functions.
+These is available function:
+
+diff(expr) - derivative of expr
+expand(expr) - expand polynomial from expr
+expand_trig(expr) - expand trigonometry of expr
+factor(expr) - factor of expr (polynomial)
+integrate(expr) - integral of expr
+limit(expr, base, target) - limit of base to target from expr
+log(expr) - natural logarithm of expr (base 'e')
+log(expr, b) - logarithm of expr with base 'b'
+series(expr) - taylor series of expr
+solve(expr) - solve equations of expr
+
+Type 'help <function>' to show more details about function.
+
+available trigonometry: sin, cos, tan, asin, acos, atan, sinh, cosh, tanh
+commons function: abs, factorial, sqrt
+commons constant: pi
+
+notes: expr using mathematical logic
+using 2*x instead of 2x or
+using 2*(x^2+x) instead of 2(x^2+x)
+expr can be stacked, ex:
+integrate(sqrt(1/x))
+
+Cheat:
+Try add .evalf() as a suffix (on the end of expression)
+    ex: log(1/4, 2).evalf()
+Try add .rewrite(func) as a suffix (on the end of expression)
+    ex: tan(x).rewrite(sin)
+Type 'help matrix' to know how to use matrix
+Type 'about' of you have a bit of curiosity.
+"""
+
+
+def exec_command(command_str):
+    if 'help' in command_str:
+        temp = re.split('\s+', command_str.strip())
+        if len(temp) == 1:
+            return DEFAULT_HELP
+        else:
+            if COMMANDS_HELP.has_key(temp[1]):
+                return COMMANDS_HELP[temp[1]]
+            else:
+                return 'help for function {} currently not available.'.format(temp[1])
+
+    elif 'about' in command_str:
+        return ABOUT_TEXT
+    elif 'plot' in command_str:
+        return """ We currently not support plot view """
+    elif 'source' in command_str:
+        return """ You cannot see the source of code via Line """
+    else:
+        if 'matrix' in command_str:
+            return parse(re.sub('matrix', 'Matrix', command_str))
+        return parse(command_str)
+
 
 def parse(expr_str):
     try:
@@ -18,11 +228,21 @@ def parse(expr_str):
 
 
 def _print_expr(expr):
-    res = expr.doit()
-    res_str = _convert_xor(str(res))
-    simp_str = _convert_xor(str(res.simplify()))
-    return_string = 'result:\n{}\n\nsimplified result:\n{}'.format(res_str, simp_str)
-    return return_string
+    try:
+        res_str = _convert_xor(str(expr))
+        simp_str = _convert_xor(str(expr.simplify()))
+        if res_str == simp_str:
+            return_string = 'result:\n{}'.format(res_str)
+        else:
+            return_string = 'result:\n{}\n\nsimplified result:\n{}'.format(res_str, simp_str)
+        return return_string
+    except AttributeError as e:
+        print(e)
+        res_str = _convert_xor(str(expr))
+        return 'result:\n{}'.format(res_str)
+    except Exception as e:
+        print(e)
+        return 'Oops something wrong'
 
 
 def _convert_xor(expr_str):
