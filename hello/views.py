e1bda96185a15276fbafe3f6900288f5fb9abd74
yusuf <yusuf@xtremax.com>
2017-02-16 17:46:48 +0700

    add help image

--- hello/views.py
+++ hello/views.py
@@ -15,7 +15,10 @@ from .worker import exec_command
 CHANNEL_ACCESS_TOKEN = os.environ['CHANNEL_ACCESS_TOKEN']
 CHANNEL_SECRET = os.environ['CHANNEL_SECRET']
 HELP_IMAGE_LINK = 'https://s19.postimg.org/50b2xcdmr/help.jpg'
-HELP_IMAGE_REVIEW_LINK = 'https://s19.postimg.org/6gmlfhgjn/help_preview.jpg'
+HELP_IMAGE_REVIEW_LINK = 'https://s19.postimg.org/6gmlfhgjn/help_preview.jpg''
+HELP_IMAGE_JHS = 'https://s19.postimg.org/9xq4fnjer/jhs.jpg'
+HELP_IMAGE_SHS_1 = 'https://s19.postimg.org/3laz5tgcj/shs1.jpg'
+HELP_IMAGE_SHS_2 = 'https://s19.postimg.org/t5d9c91qb/shs2.jpg'
 LINE_API_REPLY = "https://api.line.me/v2/bot/message/reply"
 LINE_API_PUSH = "https://api.line.me/v2/bot/message/push"
 LINE_API_HEADERS = {
@@ -56,7 +59,10 @@ def linebot(request):
             result = 'We only receive text message'
         if event.message.text.strip() == 'help':
             reply_message = ReplyMessage(event.reply_token, [Message.from_string(result), 
-            ImageMessage(HELP_IMAGE_LINK, HELP_IMAGE_REVIEW_LINK)])
+            ImageMessage(HELP_IMAGE_LINK, HELP_IMAGE_REVIEW_LINK),
+			ImageMessage(HELP_IMAGE_JHS),
+			ImageMessage(HELP_IMAGE_SHS_1),
+			ImageMessage(HELP_IMAGE_SHS_2)])
         else:
             reply_message = ReplyMessage(event.reply_token, [Message.from_string(result)])
         res = requests.post(LINE_API_REPLY, data=reply_message.to_json(), headers=LINE_API_HEADERS)
